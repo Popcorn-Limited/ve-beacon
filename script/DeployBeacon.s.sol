9e89b4d83abee7c345b4d27388e26e6696a4268a
Andrea <dinennoandrea@gmail.com>
2024-12-12 14:18:32 +0100

    veBeacon with LayerZero

--- script/DeployBeacon.s.sol
+++ script/DeployBeacon.s.sol
@@ -1,28 +1,66 @@
 // SPDX-License-Identifier: AGPL-3.0
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.25;
 
-import {CREATE3Script} from "./base/CREATE3Script.sol";
-import {VeBeacon} from "../src/VeBeacon.sol";
+import {Script, console} from "forge-std/Script.sol";
+import {VeBeaconLZ} from "../src/VeBeacon.sol";
+import {VeRecipientLZ} from "../src/VeRecipient.sol";
+import {IVotingMinimal} from "../src/interfaces/IVotingEscrow.sol";
 
-contract DeployBeaconScript is CREATE3Script {
-    constructor() CREATE3Script(vm.envString("VERSION")) {}
+// deploy beacon
+contract DeployVeBeaconLZ is Script {
+    function run() public returns (VeBeaconLZ beacon) {
+        vm.startBroadcast();
+        console.log("msg.sender:", msg.sender);
 
-    function run() external returns (VeBeacon beacon) {
-        uint256 deployerPrivateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
+        IVotingMinimal veToken = IVotingMinimal(address(0)); // veTOKEN
 
-        vm.startBroadcast(deployerPrivateKey);
+        // beacon chain LZ Endpoint
+        address lzEndpoint = address(address(0));
 
-        // deploy beacon
-        beacon = VeBeacon(
-            create3.deploy(
-                getCreate3ContractSalt("VeBeacon"),
-                bytes.concat(
-                    type(VeBeacon).creationCode,
-                    abi.encode(vm.envAddress("VOTING_ESCROW"), getCreate3Contract("VeRecipient"))
-                )
-            )
-        );
+        // deploy - sets msg.sender as owner
+        beacon = new VeBeaconLZ(veToken, lzEndpoint, msg.sender);
 
         vm.stopBroadcast();
+    }
+}
+
+// broadcast balance
+contract TransmitBalanceBeacon is Script {
+    function run() public {
+        vm.startBroadcast();
+        VeBeaconLZ beacon = VeBeaconLZ(address(0));
+        address user = address(0);
+        uint32 recipientEndpointID = 40231; // arb sepolia
+
+        (uint256 v, ) = beacon.quote(recipientEndpointID, 0, user);
+
+        // broadcast balance
+        beacon.broadcastVeBalance{value: v}(user, recipientEndpointID, 0);
+
+        vm.stopBroadcast();
+    }
+}
+
+// register new peer - add new chain
+contract SetupRecipientPeer is Script {
+    function run() public {
+        vm.startBroadcast();
+        console.log("msg.sender:", msg.sender);
+
+        uint32 recipientEndpointId = 40231; // arbitrum sepolia eID
+
+        VeBeaconLZ beacon = VeBeaconLZ(address(0));
+
+        // veRecipient address on recipient chain
+        address veRecipient = address(0);
+
+        // set recipient peer on beacon chain
+        beacon.setPeer(recipientEndpointId, addressToBytes32(veRecipient));
+
+        vm.stopBroadcast();
+    }
+
+    function addressToBytes32(address _addr) public pure returns (bytes32) {
+        return bytes32(uint256(uint160(_addr)));
     }
 }
--- src/VeBeacon.sol
+++ src/VeBeacon.sol
@@ -1,86 +1,77 @@
-// SPDX-License-Identifier: AGPL-3.0
-pragma solidity ^0.8.4;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.22;
 
+import {OApp, Origin, MessagingFee} from "@layerzerolabs/oapp-evm/contracts/oapp/OApp.sol";
+import {OptionsBuilder} from "@layerzerolabs/oapp-evm/contracts/oapp/libs/OptionsBuilder.sol";
+import {IVotingMinimal as IVotingEscrow} from "./interfaces/IVotingEscrow.sol";
+import "./base/Structs.sol";
 import "solmate/utils/SafeTransferLib.sol";
 
-import "universal-bridge-lib/UniversalBridgeLib.sol";
-
-import "./VeRecipient.sol";
-import "./base/Structs.sol";
-import "./interfaces/IVotingEscrow.sol";
-
 /// @title VeBeacon
-/// @author zefram.eth
-/// @notice Broadcasts veToken balances to other chains
-contract VeBeacon {
+/// @author Vaultcraft
+/// @notice Broadcasts veToken balances to other chains via Layer Zero
+contract VeBeaconLZ is OApp {
+    using OptionsBuilder for bytes;
+
     /// -----------------------------------------------------------------------
     /// Errors
     /// -----------------------------------------------------------------------
-
     error VeBeacon__UserNotInitialized();
 
     /// -----------------------------------------------------------------------
     /// Events
     /// -----------------------------------------------------------------------
 
-    event BroadcastVeBalance(address indexed user, uint256 indexed chainId);
+    event BroadcastVeBalance(address indexed user, uint256 indexed endpointID);
 
     /// -----------------------------------------------------------------------
     /// Constants
     /// -----------------------------------------------------------------------
 
     uint256 internal constant SLOPE_CHANGES_LENGTH = 8;
-    uint256 internal constant DATA_LENGTH = 4 + 8 * 32 + 32 + SLOPE_CHANGES_LENGTH * 64; // 4b selector + 8 * 32b args + 32b array length + SLOPE_CHANGES_LENGTH * 64b array content
+    uint256 internal constant DATA_LENGTH =
+        4 + 8 * 32 + 32 + SLOPE_CHANGES_LENGTH * 64; // 4b selector + 8 * 32b args + 32b array length + SLOPE_CHANGES_LENGTH * 64b array content
 
     /// -----------------------------------------------------------------------
     /// Immutable params
     /// -----------------------------------------------------------------------
-
     IVotingEscrow public immutable votingEscrow;
-    address public immutable recipientAddress;
 
-    /// -----------------------------------------------------------------------
-    /// Constructor
-    /// -----------------------------------------------------------------------
-
-    constructor(IVotingEscrow votingEscrow_, address recipientAddress_) {
+    constructor(
+        IVotingEscrow votingEscrow_,
+        address layerZeroEndpoint_,
+        address owner_
+    ) OApp(layerZeroEndpoint_, owner_) {
         votingEscrow = votingEscrow_;
-        recipientAddress = recipientAddress_;
     }
 
-    /// -----------------------------------------------------------------------
-    /// External functions
-    /// -----------------------------------------------------------------------
-
-    /// @notice Broadcasts a user's vetoken balance to another chain. Should use getRequiredMessageValue()
+    /// @notice Broadcasts a user's vetoken balance to another chain. Should use quote()
     /// to compute the msg.value required when calling this function.
     /// @param user the user address
-    /// @param chainId the target chain's ID
+    /// @param endpointID the target chain's LZ endpoint Id
     /// @param gasLimit the gas limit of the call to the recipient
-    /// @param maxFeePerGas the max gas price used, only relevant for some chains (e.g. Arbitrum)
-    function broadcastVeBalance(address user, uint256 chainId, uint256 gasLimit, uint256 maxFeePerGas)
-        external
-        payable
-    {
-        _broadcastVeBalance(user, chainId, gasLimit, maxFeePerGas);
+    function broadcastVeBalance(
+        address user,
+        uint32 endpointID,
+        uint256 gasLimit
+    ) external payable {
+        _broadcastVeBalance(user, endpointID, gasLimit);
         _refundEthBalanceIfWorthIt();
     }
 
-    /// @notice Broadcasts a user's vetoken balance to a list of other chains. Should use getRequiredMessageValue()
+    /// @notice Broadcasts a user's vetoken balance to a list of other chains. Should use quote()
     /// to compute the msg.value required when calling this function (currently only applicable to Arbitrum).
     /// @param user the user address
-    /// @param chainIdList the chain ID of the target chains
+    /// @param endpointIDList the LZ endpoint ID of the target chains
     /// @param gasLimit the gas limit of each call to the recipient
-    /// @param maxFeePerGas the max gas price used, only relevant for some chains (e.g. Arbitrum)
     function broadcastVeBalanceMultiple(
         address user,
-        uint256[] calldata chainIdList,
-        uint256 gasLimit,
-        uint256 maxFeePerGas
+        uint32[] calldata endpointIDList,
+        uint256 gasLimit
     ) external payable {
-        uint256 chainIdListLength = chainIdList.length;
-        for (uint256 i; i < chainIdListLength;) {
-            _broadcastVeBalance(user, chainIdList[i], gasLimit, maxFeePerGas);
+        uint256 len = endpointIDList.length;
+        for (uint256 i; i < len; ) {
+            _broadcastVeBalance(user, endpointIDList[i], gasLimit);
 
             unchecked {
                 ++i;
@@ -89,100 +80,101 @@ contract VeBeacon {
         _refundEthBalanceIfWorthIt();
     }
 
-    /// @notice Broadcasts multiple users' vetoken balances to a list of other chains. Should use getRequiredMessageValue()
-    /// to compute the msg.value required when calling this function (currently only applicable to Arbitrum).
-    /// @param userList the user addresses
-    /// @param chainIdList the chain ID of the target chains
-    /// @param gasLimit the gas limit of each call to the recipient
-    /// @param maxFeePerGas the max gas price used, only relevant for some chains (e.g. Arbitrum)
-    function broadcastVeBalanceMultiple(
-        address[] calldata userList,
-        uint256[] calldata chainIdList,
-        uint256 gasLimit,
-        uint256 maxFeePerGas
-    ) external payable {
-        uint256 userListLength = userList.length;
-        uint256 chainIdListLength = chainIdList.length;
-        for (uint256 i; i < userListLength;) {
-            for (uint256 j; j < chainIdListLength;) {
-                _broadcastVeBalance(userList[i], chainIdList[j], gasLimit, maxFeePerGas);
+    /// @dev Quotes the gas needed to pay for the full layer zero transaction.
+    /// @param dstEid Destination chain's LZ endpoint ID
+    /// @param gasLimit gasLimit for the execution tx
+    /// @param user to get balance of
+    /// @return nativeFee Estimated gas fee in native gas.
+    /// @return lzTokenFee Estimated gas fee in ZRO token.
+    function quote(
+        uint32 dstEid,
+        uint128 gasLimit,
+        address user
+    ) public view returns (uint256 nativeFee, uint256 lzTokenFee) {
+        // encode ve-balance payload
+        bytes memory _payload = _getVeBalancePayload(user);
 
-                unchecked {
-                    ++j;
-                }
-            }
+        // create LZ options
+        bytes memory options = _createOptions(uint128(gasLimit));
 
-            unchecked {
-                ++i;
-            }
-        }
-        _refundEthBalanceIfWorthIt();
-    }
+        // call LZ quote
+        MessagingFee memory fee = _quote(dstEid, _payload, options, false);
 
-    /// @notice Computes the msg.value needed when calling broadcastVeBalance(). Only relevant for Arbitrum.
-    /// @param chainId the target chain's ID
-    /// @param gasLimit the gas limit of the call to the recipient
-    /// @param maxFeePerGas the max gas price used, only relevant for some chains (e.g. Arbitrum)
-    /// @return the msg.value required
-    function getRequiredMessageValue(uint256 chainId, uint256 gasLimit, uint256 maxFeePerGas)
-        external
-        view
-        returns (uint256)
-    {
-        return UniversalBridgeLib.getRequiredMessageValue(chainId, DATA_LENGTH, gasLimit, maxFeePerGas);
+        return (fee.nativeFee, fee.lzTokenFee);
     }
 
     /// -----------------------------------------------------------------------
     /// Internal functions
     /// -----------------------------------------------------------------------
 
-    function _broadcastVeBalance(address user, uint256 chainId, uint256 gasLimit, uint256 maxFeePerGas)
-        internal
-        virtual
-    {
-        bytes memory data;
-        {
-            // get user voting escrow data
-            uint256 epoch = votingEscrow.user_point_epoch(user);
-            if (epoch == 0) revert VeBeacon__UserNotInitialized();
-            (int128 userBias, int128 userSlope, uint256 userTs,) = votingEscrow.user_point_history(user, epoch);
+    function _broadcastVeBalance(
+        address user,
+        uint32 endpointID,
+        uint256 gasLimit
+    ) internal virtual {
+        // encode ve-balance payload
+        bytes memory payload = _getVeBalancePayload(user);
 
-            // get global data
-            epoch = votingEscrow.epoch();
-            (int128 globalBias, int128 globalSlope, uint256 globalTs,) = votingEscrow.point_history(epoch);
+        // create LZ options
+        bytes memory options = _createOptions(uint128(gasLimit));
 
-            // fetch slope changes in the range [currentEpochStartTimestamp + 1 weeks, currentEpochStartTimestamp + (SLOPE_CHANGES_LENGTH + 1) * 1 weeks]
-            uint256 currentEpochStartTimestamp = (block.timestamp / (1 weeks)) * (1 weeks);
-            SlopeChange[] memory slopeChanges = new SlopeChange[](SLOPE_CHANGES_LENGTH);
-            for (uint256 i; i < SLOPE_CHANGES_LENGTH;) {
-                currentEpochStartTimestamp += 1 weeks;
-                slopeChanges[i] = SlopeChange({
-                    ts: currentEpochStartTimestamp,
-                    change: votingEscrow.slope_changes(currentEpochStartTimestamp)
-                });
-                unchecked {
-                    ++i;
-                }
+        // send data to recipient on target chain using LayerZero
+        _lzSend(
+            endpointID,
+            payload,
+            options,
+            MessagingFee(msg.value, 0),
+            payable(msg.sender)
+        );
+
+        emit BroadcastVeBalance(user, endpointID);
+    }
+
+    function _getVeBalancePayload(
+        address user
+    ) internal view returns (bytes memory payload) {
+        // get user voting escrow data
+        uint256 epoch = votingEscrow.user_point_epoch(user);
+        if (epoch == 0) revert VeBeacon__UserNotInitialized();
+        (int128 userBias, int128 userSlope, uint256 userTs, ) = votingEscrow
+            .user_point_history(user, epoch);
+
+        // get global data
+        epoch = votingEscrow.epoch();
+        (
+            int128 globalBias,
+            int128 globalSlope,
+            uint256 globalTs,
+
+        ) = votingEscrow.point_history(epoch);
+
+        // fetch slope changes in the range [currentEpochStartTimestamp + 1 weeks, currentEpochStartTimestamp + (SLOPE_CHANGES_LENGTH + 1) * 1 weeks]
+        uint256 currentEpochStartTimestamp = (block.timestamp / (1 weeks)) *
+            (1 weeks);
+        SlopeChange[] memory slopeChanges = new SlopeChange[](
+            SLOPE_CHANGES_LENGTH
+        );
+        for (uint256 i; i < SLOPE_CHANGES_LENGTH; ) {
+            currentEpochStartTimestamp += 1 weeks;
+            slopeChanges[i] = SlopeChange({
+                ts: currentEpochStartTimestamp,
+                change: votingEscrow.slope_changes(currentEpochStartTimestamp)
+            });
+            unchecked {
+                ++i;
             }
-
-            data = abi.encodeWithSelector(
-                VeRecipient.updateVeBalance.selector,
-                user,
-                userBias,
-                userSlope,
-                userTs,
-                globalBias,
-                globalSlope,
-                globalTs,
-                slopeChanges
-            );
         }
 
-        // send data to recipient on target chain using UniversalBridgeLib
-        uint256 requiredValue = UniversalBridgeLib.getRequiredMessageValue(chainId, DATA_LENGTH, gasLimit, maxFeePerGas);
-        UniversalBridgeLib.sendMessage(chainId, recipientAddress, data, gasLimit, requiredValue, maxFeePerGas);
-
-        emit BroadcastVeBalance(user, chainId);
+        payload = abi.encode(
+            user,
+            userBias,
+            userSlope,
+            userTs,
+            globalBias,
+            globalSlope,
+            globalTs,
+            slopeChanges
+        );
     }
 
     function _refundEthBalanceIfWorthIt() internal {
@@ -190,4 +182,24 @@ contract VeBeacon {
         if (address(this).balance < block.basefee * 21000) return; // early return if refunding ETH costs more than the refunded amount
         SafeTransferLib.safeTransferETH(msg.sender, address(this).balance);
     }
+
+    function _createOptions(
+        uint128 gasLimit
+    ) internal pure returns (bytes memory) {
+        // creates options for gas and msg.value on receiver side
+        return
+            OptionsBuilder.newOptions().addExecutorLzReceiveOption(
+                gasLimit > 0 ? gasLimit : 1e6,
+                0
+            );
+    }
+
+    // the veBeacon is never a receiver
+    function _lzReceive(
+        Origin calldata origin, // struct containing info about the message sender
+        bytes32 guid, // global packet identifier
+        bytes calldata payload, // encoded message payload being received
+        address executor, // the Executor address.
+        bytes calldata extraData // arbitrary data appended by the Executor
+    ) internal override {}
 }
